524c81021777e404bc3a962c3cf836905aafb5ba
Vineet Kumar Singh <vineetkumar.s@exilant.com>
2016-05-18 17:46:19 +0530

    Initial Commit

---
+++ RetailWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetailWorld
+//
+//  Created by Vineet Kumar Singh on 18/05/16.
+//  Copyright © 2016 Vineet Kumar Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
