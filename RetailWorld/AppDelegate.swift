232cef83f8fa5c658d43bc5ee3f48a5933d3d015
Vineet Kumar Singh <vineetkumar.s@exilant.com>
2016-05-20 15:22:09 +0530

    First Commit

--- RetailWorld/AppDelegate.swift
+++ RetailWorld/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Quickblox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+           QBSettings.setApplicationID(40946)
+           QBSettings.setAuthKey("kqw5t-9ThKhDfHZ")
+           QBSettings.setAuthSecret("zRMjEyx7KQKdV3q")
+            QBSettings.setAccountKey("P2VM31UfLyZAnxqZEwrx")
+        
         return true
     }
 
--- RetailWorld/ViewController.swift
+++ RetailWorld/ViewController.swift
@@ -7,19 +7,72 @@
 //
 
 import UIKit
-
+import Quickblox
 class ViewController: UIViewController {
 
+      var response = QBResponse()
+     var errorResponse = QBResponse()
+      var objects = QBCOCustomObject()
+//    var page = QBResponsePage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+    
+        
+        QBRequest.logInWithUserLogin("ajay", password: "12345678", successBlock: { (_, user) in
+            
+            print(user?.fullName)
+            print(user?.email)
+            
+            QBRequest.objectsWithClassName("UserName", successBlock: {(response,someObject) -> Void in print(response)
+                
+                print(someObject?.count)
+                
+                }, errorBlock: {(errorResponse) -> Void in print(errorResponse.description)
+            
+            })
+            
+            
+            }) { (_) in
+                
+               print("Error")
+        }
+        
+        
+        QBRequest.downloadFileWithID(4447910, successBlock: { (_, fileData) in
+            
+            print(fileData)
+            
+            }, statusBlock: { (_, _) in
+                
+                
+            }) { (_) in
+                
+        }
+        
+        
+//        var object = QBCOCustomObject()
+//        object.className = "Movies"
+//        // your Class name
+//        // Object fields
+//        object.fields?.setValue("Star Wars", forKey: "name")
+//        object.fields?.setValue(9.1, forKey: "rating")
+//        object.fields?.setValue(false, forKey: "documentary")
+//        object.fields?.setValue("fantasy", forKey: "genre")
+//        object.fields?.setValue("Star Wars is an American epic space opera franchise consisting of a film series created by George Lucas.", forKey: "descriptions")
+//       QBRequest.createObject(object, successBlock: {(response, object)  in
+//            // do something when object is successfully created on a server
+//        print("Success")
+//            }, errorBlock: {(errorResponse) in
+//                // error handling
+//                print( errorResponse.error!.description)
+//        })
+        
+}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
